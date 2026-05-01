fea10ab70301ba2fdad4c29264501a3bd804ae75
Your Name <you@example.com>
2026-05-01 20:31:32 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -85,7 +85,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     TreeNode *nodoNuevo = createTreeNode(key,value);
 
     while(aux != NULL){
-        if(is_equal(tree,key,aux->pair)){
+        if(is_equal(tree,key,aux->pair->key)){
             return;
         }
         if(tree-> lower_than(key,aux -> pair -> key)==1){
