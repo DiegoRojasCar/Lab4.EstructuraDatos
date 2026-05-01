84cea5fe2bfe126d74c7dceb29237183921c7b96
Your Name <you@example.com>
2026-05-01 20:48:50 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -251,7 +251,7 @@ Pair * nextTreeMap(TreeMap * tree) {
 // Finalmente retorne el par del nodo ub_node.
 
 Pair * upperBound(TreeMap * tree, void* key) {
-    TreeNode *aux = tree
+
     return NULL;
 }
 
